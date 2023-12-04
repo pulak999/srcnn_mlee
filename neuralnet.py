d0409ffc9b6e889254c873633163fbe780adc709
Pulak Mehrotra <97378386+pulak999@users.noreply.github.com>
2023-12-04 23:52:04 +0530

    Add files via upload

---
+++ neuralnet.py
@@ -0,0 +1,47 @@
+from tensorflow.keras.layers import Conv2D, Input 
+from tensorflow.keras.initializers import RandomNormal
+from tensorflow.keras.models import Model
+import tensorflow as tf
+
+
+def SRCNN935():
+    X_in = Input(shape=(None, None, 3))
+    X = Conv2D(filters=64, kernel_size=9, padding='valid', activation='relu',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X_in)
+    X = Conv2D(filters=32, kernel_size=3, padding='valid', activation='relu',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X)
+    X = Conv2D(filters=3,  kernel_size=5, padding='valid',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X)
+    X_out = tf.clip_by_value(X, 0.0, 1.0)
+    
+    return Model(X_in, X_out, name="SRCNN935")
+
+def SRCNN9335():
+    X_in = Input(shape=(None, None, 3))
+    X = Conv2D(filters=64, kernel_size=9, padding='valid', activation='relu',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X_in)
+    X = Conv2D(filters=32, kernel_size=3, padding='valid', activation='relu',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X)
+    X = Conv2D(filters=32, kernel_size=3, padding='valid', activation='relu',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X)
+    X = Conv2D(filters=3,  kernel_size=5, padding='valid',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X)
+    X_out = tf.clip_by_value(X, 0.0, 1.0)
+
+    return Model(X_in, X_out, name="SRCNN9335")
+
+def SRCNN93335():
+    X_in = Input(shape=(None, None, 3))
+    X = Conv2D(filters=64, kernel_size=9, padding='valid', activation='relu',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X_in)
+    X = Conv2D(filters=32, kernel_size=3, padding='valid', activation='relu',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X)
+    X = Conv2D(filters=32, kernel_size=3, padding='valid', activation='relu',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X)
+    X = Conv2D(filters=32, kernel_size=3, padding='valid', activation='relu',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X)
+    X = Conv2D(filters=3,  kernel_size=5, padding='valid',
+               kernel_initializer=RandomNormal(mean=0.0, stddev=0.001))(X)
+    X_out = tf.clip_by_value(X, 0.0, 1.0)
+
+    return Model(X_in, X_out, name="SRCNN93335")
---
+++ train.py
@@ -0,0 +1,75 @@
+import os
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = '3'
+
+from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.optimizers import Adam
+from utils.dataset import dataset
+from utils.common import PSNR
+from model import SRCNN
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--steps",          type=int, default=2000, help='-')
+parser.add_argument("--batch-size",     type=int, default=128,    help='-')
+parser.add_argument("--architecture",   type=str, default="935",  help='-')
+parser.add_argument("--save-every",     type=int, default=1000,   help='-')
+parser.add_argument("--save-best-only", type=int, default=1,      help='-')
+parser.add_argument("--save-log",       type=int, default=0,      help='-')
+parser.add_argument("--ckpt-dir",       type=str, default="",     help='-')
+
+FLAGS, unparsed = parser.parse_known_args()
+steps = FLAGS.steps
+batch_size = FLAGS.batch_size
+save_every = FLAGS.save_every
+save_log = (FLAGS.save_log == 1)
+save_best_only = (FLAGS.save_best_only == 1)
+
+architecture = FLAGS.architecture
+if architecture not in ["935", "9335", "93335"]:
+    raise ValueError("architecture must be 935, 9335 or 93335")
+
+ckpt_dir = FLAGS.ckpt_dir
+if (ckpt_dir == "") or (ckpt_dir == "default"):
+    ckpt_dir = f"checkpoint/SRCNN{architecture}"
+model_path = os.path.join(ckpt_dir, f"SRCNN-{architecture}.h5")
+
+# -----------------------------------------------------------
+#  Init datasets
+# -----------------------------------------------------------
+
+dataset_dir = "dataset"
+lr_crop_size = 33
+hr_crop_size = 19
+#if architecture == "935":
+    #hr_crop_size = 19
+#elif architecture == "955":
+    #hr_crop_size = 17
+
+train_set = dataset(dataset_dir, "train")
+train_set.generate(lr_crop_size, hr_crop_size)
+train_set.load_data()
+
+valid_set = dataset(dataset_dir, "validation")
+valid_set.generate(lr_crop_size, hr_crop_size)
+valid_set.load_data()
+
+
+# -----------------------------------------------------------
+#  Train
+# -----------------------------------------------------------
+
+def main():
+    model = SRCNN(architecture)
+    model.setup(optimizer=Adam(learning_rate=2e-5),
+                loss=MeanSquaredError(),
+                model_path=model_path,
+                metric=PSNR)
+
+    model.load_checkpoint(ckpt_dir)
+    model.train(train_set, valid_set, steps=steps, batch_size=batch_size,
+                save_best_only=save_best_only, save_every=save_every,
+                save_log=save_log, log_dir=ckpt_dir)
+
+
+if __name__ == "__main__":
+    main()
